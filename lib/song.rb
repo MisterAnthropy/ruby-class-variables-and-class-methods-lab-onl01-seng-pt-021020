57da11b1c996dcfa657ea3820d6400a5e917e113
Tyler Croley <tjcroley@outlook.com>
2020-03-04 01:15:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,5 +5,14 @@ class Song
   @@genres = []
   @@artists = []
   
-  def initialize()
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres = []
+    @@artists = []
+  end
+  
+    
 end
