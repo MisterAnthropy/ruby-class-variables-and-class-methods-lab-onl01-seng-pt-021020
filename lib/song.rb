52905ee5e65c11767addc2bcec64d8b256a39ef0
Tyler Croley <tjcroley@outlook.com>
2020-03-04 01:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1 
-    @@genres = []
-    @@artists = []
+    @@genres << genre
+    @@artists << artist 
   end
   def self.count
     @@count
